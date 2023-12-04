95cf3479c35a8a850537c275fbc822d2b791286d
Mats Fockaert <mats.fockaert@adbsafegate.com>
2023-12-04 21:08:56 +0100

    Day 4 Part 2

--- python/days/day4.py
+++ python/days/day4.py
@@ -6,13 +6,16 @@ class Day4(Day):
     def __init__(self, day=4):
         super().__init__(day)
 
+    def parse_input(self, data):
+        pattern = re.compile(r'Card\s+(\d+):\s+(.*)\s+\|\s+(.*)')
+        cards = [pattern.match(i).groups() for i in data if pattern.match(i)]
+        return cards
+
     def do_part_1(self, input = None):
         print(f"[PART 1]: Running part 1...")
         if input is None:
             input = self.get_input(1)
-            
-        pattern = re.compile(r'Card\s+(\d+):\s+(.*)\s+\|\s+(.*)')
-        l = [pattern.match(i).groups() for i in input if pattern.match(i)]
+        input = self.parse_input(input)
         correctGames = []
         for game, own, actual in l:
             numOwn = own.split(' ')
@@ -31,12 +34,25 @@ class Day4(Day):
 
     def do_part_2(self, input=None):
         print(f"[PART 2]: Running part 2...")
-        return None
+        if input is None:
+            input = self.get_input(2)
+        input = self.parse_input(input)
+        number_of_cards = [1 for _ in range(len(input))]
+        for card_number, card in enumerate(input):
+            won = 0
+            for number in card[0]:
+                if number in card[1]:
+                    won += 1 
+            for i in range(won):
+                print(card_number + (i + 1))
+                number_of_cards[card_number + (i + 1)] += (1 * number_of_cards[card_number])
+            print(f"Card {card_number + 1} won {won * number_of_cards[card_number]} extra cards.")
+        result = sum(number_of_cards)
+        print(f"[PART 2]: {result}")
+        return result
     def do_test(self, partNumber):
         return super().do_test(partNumber)
     def get_input(self, partNumber: int):
         return super().get_input(partNumber)
-    def parse_input(self, partNumber: int):
-        return super().parse_input(partNumber)
 
 
