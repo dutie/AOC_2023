aee1b48e4fa490a03307ea44d3523abdba532f52
dutie <mats.f.me@gmail.com>
2023-12-08 04:36:59 +0100

    Update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,14 +2,5 @@
 int
 main ()
 {
-  int x = 0;
-  if (x == 5)
-    {
-      std::cout << "x is 5" << std::endl;
-    }
-  else
-    {
-      std::cout << "x is not 5" << std::endl;
-    }
   return 0;
 }
